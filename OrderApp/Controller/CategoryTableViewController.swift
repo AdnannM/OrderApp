ce3d5c5cf87b0d0daa5e1d9d331eb0f3880b0579
AdnannM <48089787+AdnannM@users.noreply.github.com>
2021-12-25 12:10:07 +0100

    Network Manager and fetch Categories data

--- OrderApp/Controller/CategoryTableViewController.swift
+++ OrderApp/Controller/CategoryTableViewController.swift
@@ -9,81 +9,64 @@ import UIKit
 
 class CategoryTableViewController: UITableViewController {
 
+    
+    var categories = [String]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+        
+        fetchCategories()
     }
+    
+    private func fetchCategories() {
+        MenuController.shared.fetchCategories { result in
+            switch result {
+            case.success(let category):
+                self.updateUI(with: category)
+            case .failure(let error):
+                self.displayError(error, title: "Failed To Fetch Categories")
+            }
+        }
+    }
+    
+    private func updateUI(with categories: [String]) {
+        DispatchQueue.main.async {
+            self.categories = categories
+            self.tableView.reloadData()
+        }
+    }
+    
+    private func displayError(_ error: Error, title: String) {
+        DispatchQueue.main.async {
+            let alert = UIAlertController(title: title, message: error.localizedDescription, preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "Dismiss", style: .cancel, handler: nil))
+            self.present(alert, animated: true, completion: nil)
+        }
+    }
+}
 
-    // MARK: - Table view data source
-
+ // MARK: - TableView
+extension CategoryTableViewController {
     override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
+        return 1
     }
-
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
+        return categories.count
     }
-
-    /*
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
-        // Configure the cell...
-
+        let cell = tableView.dequeueReusableCell(withIdentifier: "categoryCell", for: indexPath)
+        configureCell(cell, forCategoryAt: indexPath)
         return cell
     }
-    */
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
+    
+    func configureCell(_ cell: UITableViewCell, forCategoryAt indexPath: IndexPath) {
+        let category = categories[indexPath.row]
+        cell.textLabel?.text = category.capitalized
+        cell.textLabel?.font = UIFont(name: "Avenir Next", size: 20)
     }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
+    
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 80
     }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
---
+++ OrderApp/NetworkManager/MenuController.swift
@@ -0,0 +1,94 @@
+//
+//  MenuController.swift
+//  OrderApp
+//
+//  Created by Adnann Muratovic on 25.12.21.
+//
+
+import Foundation
+
+class MenuController {
+    static let shared = MenuController()
+    
+    let baseURL = URL(string: "http://localhost:8080/")
+    
+    typealias MinutesToPrepere = Int
+    
+    // Get Categories
+    func fetchCategories(completion: @escaping(Result<[String], Error>) -> Void) {
+        let categoriesURL = baseURL?.appendingPathComponent("categories")
+        let task = URLSession.shared.dataTask(with: categoriesURL!) { data, response, error in
+            if let data = data {
+                do {
+                    let jsonDecoder = JSONDecoder()
+                    let categoryResponse = try jsonDecoder.decode(CategoryResponse.self, from: data)
+                    completion(.success(categoryResponse.categories))
+                }
+                catch {
+                    completion(.failure(error))
+                }
+            } else if let error = error {
+                completion(.failure(error))
+            }
+        }
+        
+        task.resume()
+    }
+    
+    // Get MenuItems
+    func fetchMenuItems(forCategory categoryName: String, completion: @escaping(Result<[MenuItem], Error>) -> Void) {
+        let menuItemURL = baseURL?.appendingPathComponent("menu")
+        var components = URLComponents(url: menuItemURL!, resolvingAgainstBaseURL: true)!
+        components.queryItems = [
+            URLQueryItem(name: "category", value: categoryName)
+        ]
+        let menuURL = components.url!
+        
+        let task = URLSession.shared.dataTask(with: menuURL) { data, response, error in
+            if let data = data {
+                do {
+                    let jsonDecoder = JSONDecoder()
+                    let menuItemsResponse = try jsonDecoder.decode(MenuResponse.self, from: data)
+                    completion(.success(menuItemsResponse.items))
+                }
+                catch {
+                    completion(.failure(error))
+                }
+            } else if let error = error {
+                completion(.failure(error))
+            }
+        }
+        
+        task.resume()
+    }
+    
+    // Post Order
+    func submitOrder(forMenuItems menuID: [Int], completion: @escaping(Result<MinutesToPrepere, Error>) -> Void) {
+        let orderURL = baseURL?.appendingPathComponent("order")
+        var request = URLRequest(url: orderURL!)
+        request.httpMethod = "POST"
+        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        
+        let data = ["menuId" : menuID]
+        let jsonEncoder = JSONEncoder()
+        let jsonData = try? jsonEncoder.encode(data)
+        request.httpBody = jsonData
+        
+        let task = URLSession.shared.dataTask(with: request) { data, response, error in
+            guard let data = data else {
+                return
+            }
+            
+            do {
+                let jsonDecoder = JSONDecoder()
+                let orderResponse = try jsonDecoder.decode(OrderResponse.self, from: data)
+                completion(.success(orderResponse.prepTime))
+            }
+            catch {
+                completion(.failure(error))
+            }
+        }
+        
+        task.resume()
+    }
+ }
